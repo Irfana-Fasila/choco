b3260a54e41e6e93653aa41352642a8f37115805
irfana140 <irfanafasilak140@gmail.com>
2024-06-21 18:08:05 +0530

    update web.php

---
+++ resources/views/home/footer.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <footer>
+    <div class="container">
+        <p>
+          &copy; <span id="displayYear"></span> All Rights Reserved By
+          <a href="https://html.design/">Web Tech Knowledge</a>
+        </p>
+      </div>
+    </footer>
+</body>
+</html>
